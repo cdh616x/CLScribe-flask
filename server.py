fd6b2250f36dd7b3d63f327ad929da32c8c14a95
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-17 23:26:34 -0500

    Initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/review", methods=["post"])
+def review():
+    x = request.form["filename"]
+    y = request.form["greeting"]
+    z = request.form["opening"]
+    a = request.form["closing"]
+    document_components = [y, z, a]
+    with open("./output/" + x + ".txt", "w") as letter:
+        for component in document_components:
+            letter.write(component + "\n\n")
+    return x
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
