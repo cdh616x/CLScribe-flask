347515d676b892bb148473f885cf475d4261bca9
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-02-21 17:54:25 -0500

    added copyright and other text, styles

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request
 from docx import Document
-import smtplib
-
-
-
+from datetime import date
 
 app = Flask(__name__)
 
+today = date.today()
+year = today.year
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", year=year)
 
 
 @app.route("/review", methods=["post", "get"])
 def review():
+
     document = Document()
+
     file_name = request.form["filename"].strip()
     greeting = request.form["greeting"]
     opening = request.form["opening"]
@@ -23,12 +24,16 @@ def review():
     personal = request.form["personal"]
     closing = request.form["closing"]
     farewell = request.form["farewell"]
+
     document_components = [greeting, opening, qualifications, personal, closing, farewell]
+
     for component in document_components:
         document.add_paragraph(component)
         document.save("./static/" + file_name + "_cover_letter.docx")
+
     return render_template("review.html", file_name=file_name, greeting=greeting, opening=opening,
-                           qualifications=qualifications, personal=personal, closing=closing, farewell=farewell, href="./static/" + file_name + "_cover_letter.docx")
+                           qualifications=qualifications, personal=personal, closing=closing, farewell=farewell,
+                           year=year, href="./static/" + file_name + "_cover_letter.docx")
 
 
 if __name__ == "__main__":
